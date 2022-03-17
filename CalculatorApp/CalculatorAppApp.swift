d11a3c2d99f50ec3c3ca55570cb3f32a40c1be03
aylm11 <aliylmzofficial@hotmail.com>
2022-03-17 13:22:47 +0300

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Ali YILMAZ on 17.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
